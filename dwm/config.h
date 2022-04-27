208eb5979e362e4d6b5e21302a02377b8bd80a4e
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-04-27 14:56:56 +0000

    new theme and barpadding

--- dwm/config.h
+++ dwm/config.h
@@ -1,13 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 12;	/* gaps between windows */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=20" };
-static const char dmenufont[]       = "monospace:size=12";
+static const unsigned int borderpx      = 1;        /* border pixel of windows */
+static const unsigned int snap      	= 32;       /* snap pixel */
+static const unsigned int gappx     	= 12;	/* gaps between windows */
+static const int showbar            	= 1;        /* 0 means no bar */
+static const int topbar             	= 1;        /* 0 means bottom bar */
+static const int vertpad	    	= 12;	/* vertical padding of bar */
+static const int sidepad 	    	= 12; 	/* horizontal padding of bar */
+static const char *fonts[]          	= { "monospace:size=12", "fontawesome:size=20" };
+static const char dmenufont[]       	= "monospace:size=12";
 static const char col_gray1[]       	= "#21124b"; // fdaffa  222222
 static const char col_gray2[]       	= "#262626";
 static const char col_gray3[]       	= "#bbbbbb";
@@ -22,7 +24,6 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-// static const char *tags[] = { "\uf120", "\uf121", "3", "4", "5", "\uf269" };
 static const char *tags[] = { "\uf26c", "\uf120", "\uf121", "\uf392", "\uf028", "\uf269" };
 
 
@@ -31,9 +32,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title   	   tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       	   0,            1,           -1 },
+	{ "Firefox",  NULL,       "firefox",       7 << 6,       0,           -1 },
 };
 
 /* layout(s) */
@@ -63,14 +64,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *firefoxcmd[]  = { "st", "-e", "firefox", NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
 
 #include "shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_f, 	   spawn,          {.v = firefoxcmd } },
+	{ MODKEY,                       XK_f, 	   spawn,          {.v = firefoxcmd } },
 	// { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
